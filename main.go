278d3b6dc6693fc6db51f73699ac7cecc9338818
RobertoSuarez <electrosonix12@gmail.com>
2024-02-10 22:54:07 -0500

    otro endpoint

--- main.go
+++ main.go
@@ -13,5 +13,9 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	app.Get("/hola", func(c *fiber.Ctx) error {
+		return c.SendString("Hola, Mundo! / hola")
+	})
+
 	log.Println(app.Listen(":3000"))
 }
