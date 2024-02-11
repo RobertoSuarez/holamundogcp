0eaa401727f606c6c867223f529165bbbc9b4e14
RobertoSuarez <electrosonix12@gmail.com>
2024-02-10 20:00:20 -0500

    hola, mundo gcp

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Println(app.Listen(":3000"))
+}
