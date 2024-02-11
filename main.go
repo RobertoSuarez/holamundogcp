e048f5744b46e47afc1c8e34d261d28d08676cfe
RobertoSuarez <electrosonix12@gmail.com>
2024-02-11 12:40:27 -0500

    configuración del puerto

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
 
+	viper.SetDefault("PORT", 3000)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, World! con fiber, viper y go")
 	})
 
 	app.Get("/hola", func(c *fiber.Ctx) error {
 		return c.SendString("Hola, Mundo! / hola")
 	})
 
-	log.Println(app.Listen(":3000"))
+	log.Println(app.Listen(":" + viper.GetString("PORT")))
 }
